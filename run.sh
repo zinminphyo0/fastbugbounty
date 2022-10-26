ad18af50836ae019e5fc1dab7b5cd0841e1584e5
zinminphyo0 <74085414+zinminphyo0@users.noreply.github.com>
2022-10-26 01:22:08 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ subfinder -d $1 -all > 2.txt
 cat *.txt | sort -u > subs.txt
 httpx-toolkit -l subs.txt -o http-subs.txt -t 150
 nuclei -t cves/ -t default-logins/ -t exposed-panels/ -t takeovers/ -t vulnerabilities/ -t iot/ -l http-subs.txt -es info -etags ssl -rl 250 -bs 50
-nuclei -t exposures/ -t misconfiguration/ -t miscellaneous/ -l http-subs.txt -es info -etags ssl -rl 250 -bs 50
+nuclei -t exposures/ -t misconfiguration/ -t mytemplates/ -t miscellaneous/ -l http-subs.txt -es info -etags ssl -rl 250 -bs 50
