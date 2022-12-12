bbaa1d20629e75058d9d484e3d3a72353906c4d4
zinminphyo0 <74085414+zinminphyo0@users.noreply.github.com>
2022-12-12 00:47:22 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ subfinder -d $1 -all -silent > 2.txt
 cat *.txt | sort -u > subs.txt
 httpx -l subs.txt -o http-subs.txt -t 150 -silent
 nuclei -t ~/nuclei-templates/cves/ -t ~/nuclei-templates/default-logins/ -t ~/nuclei-templates/exposed-panels/ -t ~/nuclei-templates/takeovers/ -t ~/nuclei-templates/vulnerabilities/ -t ~/nuclei-templates/iot/ -l http-subs.txt -as -es info -etags ssl -rl 250 -si 20 -bs 50 -c 300 -silent -o vuln1.txt
-nuclei -t ~/nuclei-templates/exposures/ -t ~/nuclei-templates/misconfiguration/ -t ~/nuclei-templates/miscellaneous/ -l http-subs.txt -as -es info -etags ssl -rl 250 -si 20 -bs 50 -silent -o vuln2.txt -c 300
+nuclei -t ~/fuzzing-templates/ -t ~/nuclei-templates/exposures/ -t ~/nuclei-templates/misconfiguration/ -t ~/nuclei-templates/miscellaneous/ -l http-subs.txt -as -es info -etags ssl -rl 250 -si 20 -bs 50 -silent -o vuln2.txt -c 300
